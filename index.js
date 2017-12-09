a51ec9967911204eeb907516b27b0a2893e0ea65
Humza Joseph <humza1born@gmail.com>
2017-12-09 23:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = 70
 if (name = "susan"){
   console.log("Name is Corect")
 } else {
-  console.log("Expected it to be susan")
+  console.log("Expected it to be Susan")
 }
 // Don't worry about this
 module.exports = { name, height
